e081e80c73465ca5ffe264dc74c77dce3ccc24f8
nickbanders <nickbanders@gmail.com>
2022-03-10 16:15:52 -0600

    added validators

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,4 @@
-const { Comment, Pizza } = require('../models');
+const { Comment, Pizza } = require("../models");
 
 const commentController = {
   // add comment to pizza
@@ -12,35 +12,39 @@ const commentController = {
           { new: true }
         );
       })
-      .then(dbPizzaData => {
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.json(err));
+      .catch((err) => res.json(err));
   },
 
   // add reply to comment
   addReply({ params, body }, res) {
-    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
-      .then(dbPizzaData => {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.json(err));
+      .catch((err) => res.json(err));
   },
 
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
-      .then(deletedComment => {
+      .then((deletedComment) => {
         if (!deletedComment) {
-          return res.status(404).json({ message: 'No comment with this id!' });
+          return res.status(404).json({ message: "No comment with this id!" });
         }
         return Pizza.findOneAndUpdate(
           { _id: params.pizzaId },
@@ -48,14 +52,14 @@ const commentController = {
           { new: true }
         );
       })
-      .then(dbPizzaData => {
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.json(err));
+      .catch((err) => res.json(err));
   },
   // remove reply
   removeReply({ params }, res) {
@@ -64,9 +68,9 @@ const commentController = {
       { $pull: { replies: { replyId: params.replyId } } },
       { new: true }
     )
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.json(err));
-  }
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = commentController;
